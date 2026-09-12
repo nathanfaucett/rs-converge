2e814df1c9990a8290ffb9429eebe0d6fd6c304a
Nathan Faucett <nathanfaucett@gmail.com>
2026-09-12 14:55:45 -0400

    Remove the redb-automerge feature alias

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ extern crate alloc;
 pub use db_engine as engine;
 #[cfg(feature = "automerge")]
 pub use db_engine_automerge as automerge;
-#[cfg(feature = "redb-automerge")]
-pub use db_engine_automerge as redb_automerge;
 #[cfg(feature = "redb")]
 pub use db_engine_redb as redb;
 pub use db_sql_translator as sql_translator;
